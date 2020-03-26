557c2beea48f0c14a806e4aac029227c5b50df8e
abhishek.mast3r <abhishek.mast3r@gmail.com>
2020-03-26 08:32:36 +0530

    mc header changed

--- src/app/services/httpcalls/crud-operations.service.ts
+++ src/app/services/httpcalls/crud-operations.service.ts
@@ -11,11 +11,8 @@ export class CrudOperationsService {
 
   getTransactions(token: string) {
     const apiUrl = `${ environment.baseUrl }/${ environment.epTransactions }`;
-    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded;application/json' });
-    const body = new URLSearchParams();
-    // body.set('public_token', 'public-sandbox-d8719373-ac7f-4471-8234-0ca390ee6a84');
-    body.set('public_token', token);
-    this.httpClient.post(apiUrl, body, { headers })
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    this.httpClient.post(apiUrl, { public_token: token }, { headers })
       .toPromise()
       .then((response) => {
         console.log(response);
